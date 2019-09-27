d6998e0da5de1dfc2e419209ce80bea9a5a86036
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-27 00:19:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,8 +5,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 def introduction_with_language_optional(name, language = "Ruby")
-  puts ""
-  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
